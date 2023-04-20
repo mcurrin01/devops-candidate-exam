b5d9f6006354411c9f62b9fc2f7b0d7c0417791f
mcurrin01 <currin.mark@siemens.com>
2023-04-20 11:19:48 -0400

    Added S3 bucket backend configuration

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region  = "ap-south-1" # Don't change the region
 }
 
-# Add your S3 backend configuration here
+# Add your S3 backend configuration here - done?
 resource "aws_s3_bucket" "3.devops.candidate.exam" {
 Bucket = "3.devops.candidate.exam"
 Region = "ap-south-1"
