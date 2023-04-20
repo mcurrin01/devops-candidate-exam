dcd44671551e35ae7e637e32cc6330b14943cb62
mcurrin01 <currin.mark@siemens.com>
2023-04-20 16:26:27 -0400

    fixes 5

--- main.tf
+++ main.tf
@@ -8,17 +8,17 @@ resource "aws_lambda_function" "lambda_requests" {
         function_name = "lambda_requests"
         filename      = "lambda_requests.zip"
         source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
-        role          = "DevOps-Candidate-Lambda-Role"
+        role          = aws_iam_role.lambda.arn
         runtime       = "python3.7"
         handler       = "lambda_function.lambda_handler"
         timeout       = 10
 }
 
 resource "aws_route_table" "example" {
-  vpc_id =  aws_vpc.example.id
+  vpc_id =  aws_vpc.vpc.id
     route {
     cidr_block = "10.0.42.0/24"
-    gateway_id = aws_nat_gateway.example.id
+    gateway_id = aws_nat_gateway.nat.id
     
   }
     tags = {
