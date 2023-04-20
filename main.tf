75a5f173fb9b6fbf3deb1c9dab34e3551bb4c3b5
mcurrin01 <currin.mark@siemens.com>
2023-04-20 15:58:59 -0400

    Add lamdba package/function and aws route table to main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+data "archive_file" "python_lambda_package" {  
+  type = "zip"  
+  source_file = "${path.module}/code/lambda_requests.py" 
+  output_path = "lambda_requests.zip"
+}
+
+resource "aws_lambda_function" "lambda_requests" {
+        function_name = "lambda_requests"
+        filename      = "lambda_requests.zip"
+        source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
+        role          = aws_iam_role.lambda_role.arn
+        runtime       = "python3.7"
+        handler       = "lambda_function.lambda_handler"
+        timeout       = 10
+}
+
+resource "aws_route_table" "example" {
+  vpc_id = vpc-00bf0d10a6a41600c
+    route {
+    cidr_block = "10.0.42.0/24"
+    gateway_id = nat-04bad8be564a37c70
+    
+  }
+}
+
